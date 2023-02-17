51b3f348862b20c86f7b483ef2f5c0eb140045a6
Charles Leclerc <chl.charlesleclerc@gmail.com>
2023-02-17 11:30:08 +0100

    updating data analysis prompt

--- my_utils/data_viz.py
+++ my_utils/data_viz.py
@@ -1,4 +1,4 @@
-#TODO: Add scaler recognition with pd.skew() values 
+#TODO: Add scaler recognition with pd.skew() values and IQR
 #TODO: Add threshold of categorical features
 #TODO: Update title with subtitle
 
@@ -8,7 +8,22 @@ from statsmodels.graphics.gofplots import qqplot
 from statsmodels.graphics.mosaicplot import mosaic
 import pandas as pd
 
-def mosaic_plot(df : pd.DataFrame , X : str , y : str , ax):
+def count_outliers(data : pd.Series, threshold : int = 3) -> float:
+
+	# Select the first quantile
+	q1 = data.quantile(.25)/threshold
+
+	# Select the third quantile
+	q3 = data.quantile(.75)*threshold
+
+	# Create a mask inbeetween q1 & q3
+	mask = data.between(q1, q3)
+
+	# Filtering the initial dataframe with a mask
+	iqr = data.loc[~mask]
+	return iqr.shape[0]
+
+def mosaic_plot(df : pd.DataFrame , X : str , y : str , ax) -> mosaic:
     '''Function to plot a mosaic plot using statsmodels.
     Base color is the one for matplotlib'''
     
@@ -25,7 +40,7 @@ def mosaic_plot(df : pd.DataFrame , X : str , y : str , ax):
     #Mosaic plot
     mosaic(df, [y, X],properties=props,labelizer = labelizer, ax=ax)
     
-def is_cat(data : pd.Series, percentage_cat : int | float = 0.09):
+def is_cat(data : pd.Series, percentage_cat : int | float = 0.09) -> bool :
     if not isinstance(data,pd.Series):
         raise TypeError(f'data should be a pd.Series')
     
@@ -91,6 +106,8 @@ def quick_check(df : pd.DataFrame, target : str, classification : bool = None, p
         if not isinstance(to_drop, list):
             raise TypeError('to_drop type must be list of string')
     
+    col_info = {col_name:dict() for col_name in df.columns} 
+    
     #Target review
     check_nan =f'{round(df[target].isna().sum()*100,2)}% of'
     
@@ -103,27 +120,59 @@ def quick_check(df : pd.DataFrame, target : str, classification : bool = None, p
     else :
         data_type = 'categorical' if classification else 'continuous'
     
-    
     #Target_overview
+    target_type = df[target].dtype.name
     prompt = '\u0332'.join('Target overview:')
     print(f'{prompt}\n')
     print(f'''  The target is {target}.
   It is composed of {check_nan} np.nan.
-  Its dtypes is {df[target].dtype.name}.
+  Its dtypes is {target_type}.
   Without specifying an estimator_job, the target will be considered as {data_type}.\n
 ''')
     
-    #Columns overview
-    prompt = '\u0332'.join('Features overview:')
-    cat_feats = [cat_feat for cat_feat in df.columns if is_cat(df[cat_feat])]
-    num_feats = [num_feat for num_feat in df.columns if not is_cat(df[num_feat])]
+    col_info[target]['check_nan'] = check_nan
+    col_info[target]['dtype'] = target_type
     
+    #Columns overview
+    cat_feats = []
+    num_feats = []
+    robust_list = []
+    minmax_list = []
+    standard_list = []
+    for feature in df.columns:
+        col_info[feature]['dtype'] = df[feature].dtype.name
+        col_info[feature]['nan'] = df[feature].isna().sum()        
+
+        if is_cat(df[feature]):
+            cat_feats.append(feature)
+            col_info[feature]['type'] = 'categorical'
+        else: 
+            num_feats.append(feature)
+            col_info[feature]['type'] = 'continuous'
+            col_info[feature]['outliers'] = count_outliers(df[feature])
+            col_info[feature]['skew'] = df[feature].skew()
+            col_info[feature]['kurt'] = df[feature].kurt()            
+            if abs(col_info[feature]['skew']) > 2 or \
+            abs(col_info[feature]['kurt']) > 7:
+                col_info[feature]['scaler_type'] = 'MinMax'
+                minmax_list.append(feature)
+            elif col_info[feature]['outliers'] == 0:
+                col_info[feature]['scaler_type'] = 'StandardScaler'
+                standard_list.append(feature)
+            else:
+                col_info[feature]['scaler_type'] = 'RobustScaler'
+                robust_list.append(feature)
+
+
+
+    
+    prompt = '\u0332'.join('Features overview:')
     print(f'{prompt}\n')
     print(f'  There is {df.shape[1]} columns which are:\n')
     if cat_feats != []:
-        print(f'    - {round(len(num_feats)/df.shape[1]*100,2)}% of them seem to be numerical;\n')
+        print(f'    - {round(len(num_feats)/df.shape[1]*100,2)}% of them seem to be numerical: [{", ".join(num_feats)}];\n')
     if num_feats != []:
-        print(f'    - {round(len(cat_feats)/df.shape[1]*100,2)}% of them seem to be categorical;\n')
+        print(f'    - {round(len(cat_feats)/df.shape[1]*100,2)}% of them seem to be categorical: [{", ".join(cat_feats)}];\n')
     
     #Dtypes overview
     prompt = '\u0332'.join('Dtypes overview:')
@@ -133,28 +182,33 @@ def quick_check(df : pd.DataFrame, target : str, classification : bool = None, p
         columns_in_dtypes = df.dtypes[df.dtypes==d_type].index
         num_d_types = round(len(columns_in_dtypes)/df.shape[1]*100,2)
         print(f'    - {d_type} corresponds to {num_d_types}% of columns and includes columns: {", ".join(columns_in_dtypes)}\n')
-        
     
     #Nan overview    
     prompt = '\u0332'.join('Nan overview:')
     print(f'{prompt}\n')
     check_nan = round(df.isna().sum()/df.shape[0]*100,2).sort_values(ascending=False)
     filtered = check_nan[check_nan>0]
-    print(f'There are {len(filtered)} features over {len(check_nan)} ({round(check_nan[check_nan>0].shape[0]/check_nan.shape[0],2)}% of whole df) that include np.nan')    
-    print(f'\nHave a look at these features (% of nan): {", ".join([f"{i}: {str(v)}%" for i,v in filtered.items()])}')
+    print(f'There are {len(filtered)} features over {len(check_nan)} ({round(check_nan[check_nan>0].shape[0]/check_nan.shape[0],2)}% of whole df) that include np.nan\n')    
+    print(f'Have a look at these features (% of nan): {", ".join([f"{i}: {str(v)}%" for i,v in filtered.items()])}\n')
 
     #Features to drop
     prompt = '\u0332'.join('Features to_drop')
     print(f'{prompt}\n')
     super_drop = check_nan[check_nan>15]
     if len(super_drop)!=0:
-        print(f'\nYou might want to drop these features: {", ".join(super_drop.index)}')
-    print(f'\nNo features to drop found.')
+        print(f'  There might be features to drop these features: {", ".join(super_drop.index)}\n')
+    print(f'  No features to drop found.\n')
     
-    print('\n')
-    print(df.info())
-
-    print('\n')
-    print("Let's have a look at all the features")
+    #Chose scaler per features
+    prompt = '\u0332'.join('Which scale to use')
+    print(f'{prompt}\n')
+    print(f'''Among your numerical features:
+    - {len(standard_list)} should use a StandardScaler: [{", ".join(standard_list)}])
+    - {len(robust_list)} should use a RobustScaler: [{", ".join(robust_list)}])
+    - {len(minmax_list)} should use a MinMaxScaler: [{", ".join(minmax_list)}])
+''')
+    
+    prompt = '\u0332'.join("Let's have a look at all the features:")
+    print(prompt)
     if to_drop : df.drop(columns=to_drop,inplace=True)
     if plot_overview: turbo_plot(df, target, classification, num_only)
